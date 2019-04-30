3436d08591ca0eb758a0ff2b7512d9bcb4f6236d
franzmueller <franzmueller@users.noreply.github.com>
2019-04-30 14:10:59 +0200

    add timeanalysis, which counts in which hour of the day outliers are happening.

---
+++ src/main/java/org/infai/senergy/benchmark/smartmeter/util/TimeRow.java
@@ -0,0 +1,231 @@
+package org.infai.senergy.benchmark.smartmeter.util;
+
+import java.io.Serializable;
+
+public class TimeRow implements Serializable {
+    private String METER_ID;
+    private int one;
+    private int two;
+    private int three;
+    private int four;
+    private int five;
+    private int six;
+    private int seven;
+    private int eight;
+    private int nine;
+    private int ten;
+    private int eleven;
+    private int twelfe;
+    private int thirteen;
+    private int fourteen;
+    private int fiveteen;
+    private int sixteen;
+    private int seventeen;
+    private int eighteen;
+    private int nineteen;
+    private int twenty;
+    private int twentyone;
+    private int twentytwo;
+    private int twentythree;
+    private int twentyfour;
+
+    public String getMETER_ID() {
+        return METER_ID;
+    }
+
+    public void setMETER_ID(String METER_ID) {
+        this.METER_ID = METER_ID;
+    }
+
+    public int getOne() {
+        return one;
+    }
+
+    public void setOne(int one) {
+        this.one = one;
+    }
+
+    public int getTwo() {
+        return two;
+    }
+
+    public void setTwo(int two) {
+        this.two = two;
+    }
+
+    public int getThree() {
+        return three;
+    }
+
+    public void setThree(int three) {
+        this.three = three;
+    }
+
+    public int getFour() {
+        return four;
+    }
+
+    public void setFour(int four) {
+        this.four = four;
+    }
+
+    public int getFive() {
+        return five;
+    }
+
+    public void setFive(int five) {
+        this.five = five;
+    }
+
+    public int getSix() {
+        return six;
+    }
+
+    public void setSix(int six) {
+        this.six = six;
+    }
+
+    public int getSeven() {
+        return seven;
+    }
+
+    public void setSeven(int seven) {
+        this.seven = seven;
+    }
+
+    public int getEight() {
+        return eight;
+    }
+
+    public void setEight(int eight) {
+        this.eight = eight;
+    }
+
+    public int getNine() {
+        return nine;
+    }
+
+    public void setNine(int nine) {
+        this.nine = nine;
+    }
+
+    public int getTen() {
+        return ten;
+    }
+
+    public void setTen(int ten) {
+        this.ten = ten;
+    }
+
+    public int getEleven() {
+        return eleven;
+    }
+
+    public void setEleven(int eleven) {
+        this.eleven = eleven;
+    }
+
+    public int getTwelfe() {
+        return twelfe;
+    }
+
+    public void setTwelfe(int twelfe) {
+        this.twelfe = twelfe;
+    }
+
+    public int getThirteen() {
+        return thirteen;
+    }
+
+    public void setThirteen(int thirteen) {
+        this.thirteen = thirteen;
+    }
+
+    public int getFourteen() {
+        return fourteen;
+    }
+
+    public void setFourteen(int fourteen) {
+        this.fourteen = fourteen;
+    }
+
+    public int getFiveteen() {
+        return fiveteen;
+    }
+
+    public void setFiveteen(int fiveteen) {
+        this.fiveteen = fiveteen;
+    }
+
+    public int getSixteen() {
+        return sixteen;
+    }
+
+    public void setSixteen(int sixteen) {
+        this.sixteen = sixteen;
+    }
+
+    public int getSeventeen() {
+        return seventeen;
+    }
+
+    public void setSeventeen(int seventeen) {
+        this.seventeen = seventeen;
+    }
+
+    public int getEighteen() {
+        return eighteen;
+    }
+
+    public void setEighteen(int eighteen) {
+        this.eighteen = eighteen;
+    }
+
+    public int getNineteen() {
+        return nineteen;
+    }
+
+    public void setNineteen(int nineteen) {
+        this.nineteen = nineteen;
+    }
+
+    public int getTwenty() {
+        return twenty;
+    }
+
+    public void setTwenty(int twenty) {
+        this.twenty = twenty;
+    }
+
+    public int getTwentyOne() {
+        return twentyone;
+    }
+
+    public void setTwentyOne(int twentyone) {
+        this.twentyone = twentyone;
+    }
+
+    public int getTwentytwo() {
+        return twentytwo;
+    }
+
+    public void setTwentytwo(int twentytwo) {
+        this.twentytwo = twentytwo;
+    }
+
+    public int getTwentythree() {
+        return twentythree;
+    }
+
+    public void setTwentythree(int twentythree) {
+        this.twentythree = twentythree;
+    }
+
+    public int getTwentyfour() {
+        return twentyfour;
+    }
+
+    public void setTwentyfour(int twentyfour) {
+        this.twentyfour = twentyfour;
+    }
+}
