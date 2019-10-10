d35c8dd0e50354931ef8c2eb7db5932a8cf777af
franzmueller <franzmueller@users.noreply.github.com>
2019-10-10 11:39:35 +0200

    switch to batch training and stream testing

--- src/main/java/org/infai/senergy/benchmark/smartmeter/SegmentClassifier.java
+++ src/main/java/org/infai/senergy/benchmark/smartmeter/SegmentClassifier.java
@@ -1,6 +1,15 @@
 package org.infai.senergy.benchmark.smartmeter;
 
-import org.apache.spark.sql.*;
+import org.apache.spark.ml.*;
+import org.apache.spark.ml.classification.Classifier;
+import org.apache.spark.ml.classification.RandomForestClassifier;
+import org.apache.spark.ml.feature.SQLTransformer;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
@@ -10,32 +19,34 @@ import org.infai.senergy.benchmark.util.SmartmeterSchema;
 public class SegmentClassifier {
     public static void main(String[] args) {
         //Usage check
-        String errorMessage = "Usage: org.infai.senergy.benchmark.smartmeter.SegmentClassifier <logging> <hostlist> <inputTopic> <outputTopic> <sigma> <startingOffsets> <maxOffsetsPerTrigger> <shufflePartitions>\n" +
+        String errorMessage = "Usage: org.infai.senergy.benchmark.smartmeter.SegmentClassifier <logging> <hostlist> <inputTopicTraining> <inputTopicTest> <outputTopic> <sigma> <startingOffsets> <maxOffsetsPerTrigger> <shufflePartitions>\n" +
                 "logging = boolean\n" +
                 "hostlist = comma-separated list of kafka host:port\n" +
-                "inputTopic = Topic will be subscribed to.\n" +
+                "inputTopicTraining = Topic of training data.\n" +
+                "inputTopicTest = Topic of test data.\n" +
                 "outputTopic = Output topic, where values will be written to\n" +
                 "startingOffsets = Which Kafka Offset to use. Use earliest or latest\n" +
                 "maxOffsetsPerTrigger = How many messages should be consumed at once (max)\n" +
                 "shufflePartitions = How many shuffle partitions Spark should use (default is 200)";
 
-        if (args.length != 7) {
+        if (args.length != 8) {
             System.out.println(errorMessage);
             return;
         }
         //Parameter configuration
         boolean loggingEnabled;
-        String hostlist, inputTopic, outputTopic, startingOffsets;
+        String hostlist, inputTopicTraining, inputTopicTest, outputTopic, startingOffsets;
         int shufflePartitions;
         long maxOffsetsPerTrigger;
         try {
             loggingEnabled = Boolean.parseBoolean(args[0]);
             hostlist = args[1];
-            inputTopic = args[2];
-            outputTopic = args[3];
-            startingOffsets = args[4];
-            maxOffsetsPerTrigger = Long.parseLong(args[5]);
-            shufflePartitions = Integer.parseInt(args[6]);
+            inputTopicTraining = args[2];
+            inputTopicTest = args[3];
+            outputTopic = args[4];
+            startingOffsets = args[5];
+            maxOffsetsPerTrigger = Long.parseLong(args[6]);
+            shufflePartitions = Integer.parseInt(args[7]);
         } catch (Exception e) {
             System.out.println(errorMessage);
             return;
@@ -50,32 +61,25 @@ public class SegmentClassifier {
                 .config("spark.sql.streaming.checkpointLocation", "checkpoints")
                 .getOrCreate();
 
+        //TRAINING PHASE
+
         //Create DataSet representing the stream of input lines from kafka
-        Dataset<Row> ds = spark
-                .readStream()
+        Dataset<Row> trainingData = spark
+                .read()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", hostlist)
-                .option("subscribe", inputTopic)
+                .option("subscribe", inputTopicTraining)
                 .option("startingOffsets", startingOffsets)
-                .option("maxOffsetsPerTrigger", maxOffsetsPerTrigger)
                 .load();
 
         //Prepare the schema
         StructType schema = SmartmeterSchema.getSchema();
 
         //Parse Kafka value to Dataframe (via json)
-        Dataset<Row> df = ds.select(functions.from_json(ds.col("value").cast(DataTypes.StringType), schema)
+        trainingData = trainingData.select(functions.from_json(trainingData.col("value").cast(DataTypes.StringType), schema)
                 .as("data"))
                 .select("data.*");
 
-        SQLContext sqlContext = spark.sqlContext();
-        sqlContext.setConf("hostlist", hostlist);
-        sqlContext.setConf("outputTopic", outputTopic);
-
-        df.writeStream().format("org.infai.senergy.benchmark.smartmeter.util.SegmentClassifierSinkProvider").start();
-/*
-
-        //Create indexers
         StringIndexer segmentIndexer = new StringIndexer()
                 .setInputCol("SEGMENT")
                 .setOutputCol("indexedSEGMENT");
@@ -83,51 +87,47 @@ public class SegmentClassifier {
                 .setInputCol("METER_ID")
                 .setOutputCol("indexedMETER_ID");
 
-        Transformer sqlTransformer = new SQLTransformer().setStatement("SELECT CONSUMPTION, SEGMENT, indexedMETER_ID, unix_timestamp(TIMESTAMP_UTC) AS unixTIMESTAMP_UTC FROM __THIS__");
+        Transformer sqlTransformer = new SQLTransformer().setStatement("SELECT CONSUMPTION, indexedSEGMENT, indexedMETER_ID, unix_timestamp(TIMESTAMP_UTC) AS unixTIMESTAMP_UTC FROM __THIS__");
 
         //Create assembler
         String[] featuresCols = {"indexedMETER_ID", "CONSUMPTION", "unixTIMESTAMP_UTC"};
         VectorAssembler assembler = new VectorAssembler().setInputCols(featuresCols).setOutputCol("FEATURES");
 
-
-        Pipeline prep = new Pipeline().setStages(new PipelineStage[]{meterIndexer, sqlTransformer, assembler});
-        Dataset<Row> prepared = prep.fit(df).transform(df);
-
-
-        //Index data
-        df = meterIndexer.fit(df).transform(df);
-        df = timestampIndexer.fit(df).transform(df);
-        df = assembler.transform(df);
-
-
-
-        //Create training data and index segments
-        Dataset<Row> trainingData = prepared.where(df.col("SEGMENT").isNotNull());
-        trainingData = segmentIndexer.fit(trainingData).transform(trainingData);
-
-        //Create test data
-        Dataset<Row> testData = df.where(df.col("SEGMENT").isNull());
-
-        //Create classifier
         Classifier classifier = new RandomForestClassifier();
+
         Predictor predictor = classifier.setLabelCol("indexedSEGMENT");
         predictor = predictor.setFeaturesCol("FEATURES");
         predictor = predictor.setPredictionCol("PREDICTION");
 
-        PredictionModel model = predictor.fit(trainingData);
+        Pipeline trainingPipeline = new Pipeline().setStages(new PipelineStage[]{meterIndexer, segmentIndexer, sqlTransformer, assembler, predictor});
+        PipelineModel model = trainingPipeline.fit(trainingData);
 
-        Dataset segmented = model.transform(testData);
 
+        //TEST PHASE
 
-        //Write outliers to kafka
-        segmented.toJSON()
+        //Create DataSet representing the stream of input lines from kafka
+        Dataset<Row> testData = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", hostlist)
+                .option("subscribe", inputTopicTest)
+                .option("startingOffsets", startingOffsets)
+                .option("maxOffsetsPerTrigger", maxOffsetsPerTrigger)
+                .load();
+
+        //Parse Kafka value to Dataframe (via json)
+        testData = testData.select(functions.from_json(testData.col("value").cast(DataTypes.StringType), schema)
+                .as("data"))
+                .select("data.*");
+
+        model.transform(testData)
+                .toJSON()
                 .writeStream()
                 .format("kafka")
-                .option("checkpointLocation", "checkpoints/smartmeter/outlierdetecion")
                 .option("kafka.bootstrap.servers", hostlist)
                 .option("topic", outputTopic)
                 .start();
-*/
+
         // Wait for termination
         try {
             spark.streams().awaitAnyTermination();
